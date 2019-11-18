b23e1debb3e154119a1c6fc61d8dcb3cd7381708
Nate Clark <natec425@gmail.com>
2019-11-18 11:49:38 -0600

    Add code for solution

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.TextField()
+    author = models.TextField()
+    published = models.DateField()
+    genre = models.TextField()
+    in_stock = models.BooleanField()
+    description = models.TextField()
+
+
+class Transaction(models.Model):
+    datetime = models.DateTimeField()
+    action = models.TextField()
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Book, Transaction
+from django.contrib import messages
+from django.utils import timezone
 
-# Create your views here.
+
+def home(request):
+    return render(request, "book_list.html", {"books": Book.objects.all()})
+
+
+def borrow_book(request, id):
+    book = Book.objects.get(id=id)
+    if book.in_stock:
+        book.in_stock = False
+        book.save()
+        book.transaction_set.create(datetime=timezone.now(), action="CHECKOUT")
+        messages.success(request, f"Borrowed {book.title} by {book.author}")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is unavailable")
+    return redirect("home")
+
+
+def return_book(request, id):
+    book = Book.objects.get(id=id)
+    if not book.in_stock:
+        book.in_stock = True
+        book.save()
+        book.transaction_set.create(datetime=timezone.now(), action="CHECKIN")
+        messages.success(request, f"Returned {book.title} by {book.author}")
+    else:
+        messages.error(request, f"{book.title} by {book.author} is already here")
+    return redirect("home")
